0ecc9954661ecaed3fe980fd7a69603b148645df
GabriellePye <gabriellepye@rockborne.com>
2024-10-15 19:47:09 +0100

    Update streamlit.app.py
    putting json into dataframe

--- streamlit.app.py
+++ streamlit.app.py
@@ -33,5 +33,6 @@ else:
    
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
    # st.success('Merge operation completed successfully!', icon='👍')
